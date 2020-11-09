e285e48ec912c2c5d388c0d6b74121e6eb9e22d6
xavier-breuil <breuilxavier@gmail.com>
2020-11-09 07:17:57 +0100

    fix: Fix coverage settings.

--- comprendre/manage.py
+++ comprendre/manage.py
@@ -15,7 +15,25 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    IS_TESTING = 'test' in sys.argv
+
+    # Coverage needs to start before tests running.
+    if IS_TESTING:
+        import coverage
+        COV = coverage.coverage(source=['comprendre.users'])
+        COV.erase()
+        COV.start()
+
+    # Normal command execution.
     execute_from_command_line(sys.argv)
+
+    if IS_TESTING:
+        COV.stop()
+        COV.save()
+        if COV.report() < 100:
+            # If code is not 100% covered, we should return error code.
+            sys.exit(1)
 
 
 if __name__ == '__main__':
