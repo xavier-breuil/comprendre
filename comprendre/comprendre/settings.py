c806d01cfc26b7833aa828439fcecf7423e47945
xavier-breuil <breuilxavier@gmail.com>
2020-11-15 16:03:49 +0100

    conf: Setup cors headers.

--- comprendre/comprendre/settings.py
+++ comprendre/comprendre/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'drf_yasg',
+    'corsheaders',
 
     # Custom Apps
     'comprendre.users',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -158,3 +160,10 @@ REST_FRAMEWORK = {
     # Filtering
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
+
+
+# CORS config
+# TODO: config for prod when relevant.
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+    ]
